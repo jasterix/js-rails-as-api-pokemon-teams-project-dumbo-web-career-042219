281c6a13b1802a27386b38e0d09aacc1775b5034
Jasterix <ljcalixte@gmail.com>
2019-06-10 17:37:27 -0400

    with nested fetch

--- pokemon-teams-backend/app/controllers/trainers_controller.rb
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -1,8 +1,11 @@
 class TrainersController < ApplicationController
-
+  before_action :show, :index
 def index
      trainers = Trainer.all
     render json: trainers
+end
+def show
+
 end
 
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -2,50 +2,52 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   const BASE_URL = "http://localhost:3000"
-  const TRAINERS_URL = `${BASE_URL}/trainers`
+  const TRAINERS_URL = `${BASE_URL}/trainers/`
   const POKEMONS_URL = `${BASE_URL}/pokemons`
 
-const main = document.querySelector('main')
-const
-
-function getPokemons() {
-  fetch(POKEMONS_URL)
-  .then(response => response.json())
+const mainTag = document.querySelector('main')
+// const
+let result = fetch(POKEMONS_URL, {
+    method: 'get',
+  })
+  .then(resp => resp.json())
   .then(pokemons => {
-
-    console.log(pokemons);
-
-pokemons.forEach(pokemon => {
-  if pokemon.trainer_id === trainer.id
-
-  `<li>
-      ${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button>
-    </li>
-
-  `})
-})
-
-
-
-document.querySelector('ul')
-
-fetch(TRAINERS_URL)
-.then(response => response.json())
-.then(trainers => {
-    console.log(trainers);
-
-    trainers.forEach(trainer => {
-      main.innerHTML += `
-        <div class="card" data-id="${trainer.id}"><p>${trainer.name}</p>
-          <button data-trainer-id="${trainer.id}">Add Pokemon</button>``
-          <ul>POKELIST</ul>
-        </div>
-      `
+    let trainerId;
+    pokemons.forEach(pokemon => {
+      trainerId = pokemon.trainer_id;
+      // console.log(trainerId)
+      return fetch(TRAINERS_URL + trainerId) // make a 2nd request and return a promise
+      .then(resp => resp.json())
+      .then(data => {
+        // mainTag.innerHTML +=
+        data.forEach(trainer =>{
+            while(trainer.id == pokemon.trainer_id) {
+          // debugger
+          console.log(trainer)
+      }
+          // debugger
+        // })
+        })
+      })
+      // .catch(error => {
+      //   alert('Request failed', error)
+      })
     })
+  // })
 
-})
 
-
+//     trainers.forEach(trainer => {
+//       main.innerHTML += `
+//         <div class="card" data-id="${trainer.id}"><p>${trainer.name}</p>
+//           <button data-trainer-id="${trainer.id}">Add Pokemon</button>``
+//           <ul>POKELIST</ul>
+//         </div>
+//       `
+//     })
+//
+// })
+//
+//
 
 
 
